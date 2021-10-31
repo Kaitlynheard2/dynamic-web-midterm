e708da2cd91873214706334efece639b1b365bb5
MyMac <mymac@MacBook-Pro-4.local>
2021-10-31 12:52:58 -0400

    UI is almost done

--- src/components/AnalysisCard.js
+++ src/components/AnalysisCard.js
@@ -1,31 +1,69 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function AnalysisCard(tweet) {
+function AnalysisCard({ tweetInfo }) {
   const [analysisData, setAnalysisData] = useState({});
   const ANAYLSIS_API_KEY = `74f9282a0e925cc02721212041e7bc84`;
 
   useEffect(() => {
-    //if (tweetData) {
-    axios
-      .get(
-        `https://api.meaningcloud.com/sentiment-2.1?key=${ANAYLSIS_API_KEY}&lang=auto&txt=${tweetData.text}`
-      )
-      .then(function (response) {
-        console.log(response);
-        setAnalysisData(response.data);
-      })
-      .catch(function (error) {
-        console.warn(error);
-      });
-    //}
+    if (tweetInfo) {
+      axios
+        .get(
+          `https://api.meaningcloud.com/sentiment-2.1?key=${ANAYLSIS_API_KEY}&lang=auto&txt=${tweetInfo.text}`
+        )
+        .then(function (response) {
+          console.log(response);
+          setAnalysisData(response);
+        })
+        .catch(function (error) {
+          console.warn(error);
+        });
+    }
   }, [tweetInfo]);
 
+  const analysisColorString = AnalysisColor(analysisData);
+  const TweetID = tweetInfo.id;
+
   return (
-    <div>
-      <p>{tweetInfo.text}</p>
+    <div className="DataWrapper">
+      <div
+        className="AnalysisStyle"
+        style={{
+          backgroundColor: analysisColorString,
+        }}
+      ></div>
+      <p className="TextStyle">{tweetInfo.text}</p>
+      <div className="TweetButtonStyle">
+        <a
+          className="TweetButtonLinkStyle"
+          id="link"
+          href="https://twitter.com/statuses/"
+        >
+          Go To Tweet
+        </a>
+      </div>
     </div>
   );
+}
+
+export function AnalysisColor(Analysis) {
+  if (!Analysis.data) {
+    return null;
+  }
+  switch (Analysis.data.score_tag) {
+    case "P+":
+      return `rgb(${29}, ${161}, ${242})`;
+    case "P":
+      return `rgb(${25}, ${140}, ${212})`;
+    case "NEU":
+      return `rgb(${21}, ${115}, ${173})`;
+    case "N":
+      return `rgb(${15}, ${81}, ${122})`;
+    case "N+":
+      return `rgb(${10}, ${57}, ${87})`;
+    default:
+      return `rgb(${101}, ${119}, ${134})`;
+  }
 }
 
 export default AnalysisCard;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,26 +3,14 @@ import axios from "axios";
 import { useLocation } from "react-router-dom";
 import AnalysisCard from "../components/AnalysisCard";
 
-const TWITTER_API_KEY = `MFRmMIIjwGpm8sdBQrskD7uug`;
-const TWITTER_API_SECTRET_KEY = `ikzSpUmDLhCIM3rZQWrVynMf12gxSj5ZcAawANWd1LfNY9qoZL`;
-
-function useQuery() {
-  return new URLSearchParams(useLocation().search);
-}
+//const TWITTER_API_KEY = `MFRmMIIjwGpm8sdBQrskD7uug`;
+//const TWITTER_API_SECTRET_KEY = `ikzSpUmDLhCIM3rZQWrVynMf12gxSj5ZcAawANWd1LfNY9qoZL`;
 
 function Home() {
   const [tweetData, setTweet] = useState();
 
   const BEARER_TOKEN = `AAAAAAAAAAAAAAAAAAAAAIZfUwEAAAAAmUXjvCgh%2Fux7VSX4tTwqOid4WSo%3D4Pd9V02o0zokvvmz9J7MSXH0FtRA8IrnuEXY4TQgqgnzYwwt34`;
-  const twitterURL = `https://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/1163912602931662848/tweets`;
   // cors anywhere link: https://cors-anywhere.herokuapp.com/corsdemo
-
-  let query = useQuery();
-
-  //useEffect(() => {
-  //const tweetKey = query.get("tweet");
-  //setTweet(tweetKey);
-  //}, [query]);
 
   useEffect(() => {
     let config = {
@@ -44,38 +32,44 @@ function Home() {
       });
   }, []);
 
-  /*
-  const {
-    tweet1,
-    tweet2,
-    tweet3,
-    tweet4,
-    tweet5,
-    tweet6,
-    tweet7,
-    tweet8,
-    tweet9,
-    tweet10,
-  } = useMemo(() => {
-    if (!tweet) return {};
-    return {
-      tweet1: tweet[0].text,
-      tweet2: tweet[1].text,
-      tweet3: tweet[2].text,
-      tweet4: tweet[3].text,
-      tweet5: tweet[4].text,
-      tweet6: tweet[5].text,
-      tweet7: tweet[6].text,
-      tweet8: tweet[7].text,
-      tweet9: tweet[8].text,
-      tweet10: tweet[9].text,
-    };
-  }, [tweet]);
-*/
   return (
-    <div>
-      {tweetData &&
-        tweetData.map((tweet, i) => <AnalysisCard key={i} tweet={tweet} />)}
+    <div className="Wrapper">
+      <header className="HeaderStyle">The Book Of Basi</header>
+      <div>
+        <div className="PolarityKeyCard">
+          <p className="PolarityTitle">Polarity Key</p>
+          <div className="PolarityCategory">
+            <p>P+ (Strong Positive)</p>
+            <div className="PPlusStyle"></div>
+          </div>
+          <div className="PolarityCategory">
+            <p>P (Positive)</p>
+            <div className="PStyle"></div>
+          </div>
+          <div className="PolarityCategory">
+            <p>NEU (Neutral)</p>
+            <div className="NEUStyle"></div>
+          </div>
+          <div className="PolarityCategory">
+            <p>N (Negative)</p>
+            <div className="NStyle"></div>
+          </div>
+          <div className="PolarityCategory">
+            <p>N+ (Strong Negative)</p>
+            <div className="NPlusStyle"></div>
+          </div>
+          <div className="PolarityCategory">
+            <p>NONE (Without Polarity)</p>
+            <div className="NonPolarStyle"></div>
+          </div>
+        </div>
+      </div>
+      <div className="ContentWrapper">
+        {tweetData &&
+          tweetData.map((tweetInfo, i) => (
+            <AnalysisCard key={i} tweetInfo={tweetInfo} />
+          ))}
+      </div>
     </div>
   );
 }
